a96dcd1469566b18b25a477c2a24b4208bdb4c24
Samuel Erdtman <erdtman@spotify.com>
2019-06-08 23:35:42 +0200

    fixed sorting

--- src/main/webapp/script/implementations.js
+++ src/main/webapp/script/implementations.js
@@ -3,8 +3,8 @@
 const source   = document.getElementById("implementations_template").innerHTML;
 const template = Handlebars.compile(source);
 
-const sorted_scim_v1_implementations = scim_v1_implementations.sort((a,b) => a.project_name > b.project_name);
-const sorted_scim_v2_implementations = scim_v1_implementations.sort((a,b) => a.project_name > b.project_name);
+scim_v1_implementations.implementations = scim_v1_implementations.implementations.sort((a,b) => a.project_name > b.project_name);
+scim_v2_implementations.implementations = scim_v1_implementations.implementations.sort((a,b) => a.project_name > b.project_name);
 
 const scim_v1_implementations_html = template(sorted_scim_v1_implementations);
 const scim_v2_implementations_html = template(sorted_scim_v2_implementations);
