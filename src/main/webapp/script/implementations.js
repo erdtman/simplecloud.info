697c60f198170011c22c883ef6505a3e08321265
Samuel Erdtman <erdtman@spotify.com>
2019-07-21 23:52:23 +0200

    fixed v2 implementations listed twice and corrected some v1 implementation names

--- src/main/webapp/script/implementations.js
+++ src/main/webapp/script/implementations.js
@@ -6,7 +6,7 @@ const template = Handlebars.compile(source);
 scim_v1_implementations.implementations = scim_v1_implementations.implementations.sort((a,b) => a.project_name > b.project_name);
 scim_v2_implementations.implementations = scim_v2_implementations.implementations.sort((a,b) => a.project_name > b.project_name);
 
-const scim_v1_implementations_html = template(scim_v2_implementations);
+const scim_v1_implementations_html = template(scim_v1_implementations);
 const scim_v2_implementations_html = template(scim_v2_implementations);
 
 document.getElementById("scim_v1_implementations").innerHTML = scim_v1_implementations_html;
