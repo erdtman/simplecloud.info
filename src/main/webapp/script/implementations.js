3d271c33386a5a105704ed10ad85e0eb866d4392
Samuel Erdtman <erdtman@spotify.com>
2019-09-10 22:52:51 +0200

    case insencitive sort

--- src/main/webapp/script/implementations.js
+++ src/main/webapp/script/implementations.js
@@ -3,8 +3,8 @@
 const source   = document.getElementById("implementations_template").innerHTML;
 const template = Handlebars.compile(source);
 
-scim_v1_implementations.implementations = scim_v1_implementations.implementations.sort((a,b) => a.project_name > b.project_name);
-scim_v2_implementations.implementations = scim_v2_implementations.implementations.sort((a,b) => a.project_name > b.project_name);
+scim_v1_implementations.implementations = scim_v1_implementations.implementations.sort((a,b) => a.project_name.toUpperCase() > b.project_name.toUpperCase());
+scim_v2_implementations.implementations = scim_v2_implementations.implementations.sort((a,b) => a.project_name.toUpperCase() > b.project_name.toUpperCase());
 
 const scim_v1_implementations_html = template(scim_v1_implementations);
 const scim_v2_implementations_html = template(scim_v2_implementations);
